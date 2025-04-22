bf0c4c5224b554922becb33aa91047cd5d36eea9
luciana87 <ch.luciana87@gmail.com>
2025-04-22 16:52:42 -0300

    Refactor styles

--- frontend/challenge-factor-it/src/app/components/user/vips/vips.component.ts
+++ frontend/challenge-factor-it/src/app/components/user/vips/vips.component.ts
@@ -3,11 +3,12 @@ import { UserService } from '../../services/user.service';
 import { User } from '../../../models/User';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { BooleanToSiNoPipe } from '../../../pipes/boolean-to-si-no.pipe'
 
 @Component({
   selector: 'app-vips',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, BooleanToSiNoPipe],
   templateUrl: './vips.component.html',
   styleUrl: './vips.component.css'
 })
